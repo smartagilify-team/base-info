335477edc971bf5030cc8fcb2955678d08e66c9b
amirfathi37 <fathiamir37@gmail.com>
2023-05-09 17:08:42 +0200

    feat(base-info-detail): unit test

--- src/test/java/com/smartagilify/baseinfo/services/BaseInfoDetailServiceImplTest.java
+++ src/test/java/com/smartagilify/baseinfo/services/BaseInfoDetailServiceImplTest.java
@@ -3,10 +3,10 @@ package com.smartagilify.baseinfo.services;
 import com.smartagilify.baseinfo.dtos.BaseInfoDetailDTO;
 import com.smartagilify.baseinfo.entities.BaseInfo;
 import com.smartagilify.baseinfo.entities.BaseInfoDetail;
-import com.smartagilify.baseinfo.mappers.BaseInfoDetailMapper;
 import com.smartagilify.baseinfo.repositories.BaseInfoDetailRepository;
 import com.smartagilify.baseinfo.repositories.BaseInfoRepository;
 import com.smartagilify.core.enumerations.EN_ACTION_TYPE;
+import com.smartagilify.core.exceptions.BusinessException;
 import com.smartagilify.core.model.InputDTO;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,12 +14,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class BaseInfoDetailServiceImplTest {
@@ -59,6 +60,70 @@ class BaseInfoDetailServiceImplTest {
 
         verify(baseInfoRepository).findById(baseInfo.getId());
         verify(baseInfoDetailRepository).findById(baseInfoDetailDTO.getParentId());
+    }
+
+    @Test
+    void receiveBaseInfoNotFoundException() {
+        BaseInfoDetailDTO baseInfoDetailDTO = new BaseInfoDetailDTO();
+        baseInfoDetailDTO.setId(100L);
+        baseInfoDetailDTO.setTitle("samand");
+        baseInfoDetailDTO.setCode("IKH-SMD");
+        baseInfoDetailDTO.setBaseInfoId(1L);
+
+        InputDTO<BaseInfoDetailDTO> inputDTO = new InputDTO<>();
+        inputDTO.setActionType(EN_ACTION_TYPE.SAVE);
+        inputDTO.setUserId(17L);
+        inputDTO.setData(baseInfoDetailDTO);
+
+        when(baseInfoRepository.findById(any())).thenReturn(Optional.empty());
+
+        assertThrows(BusinessException.class, () -> service.save(inputDTO));
+        verify(baseInfoDetailRepository, never()).save(any());
+    }
+
+    @Test
+    void receiveParentNotFoundException() {
+        BaseInfo baseInfo = BaseInfo.builder().id(1L).build();
+        BaseInfoDetailDTO baseInfoDetailDTO = new BaseInfoDetailDTO();
+        baseInfoDetailDTO.setId(100L);
+        baseInfoDetailDTO.setTitle("samand");
+        baseInfoDetailDTO.setCode("IKH-SMD");
+        baseInfoDetailDTO.setBaseInfoId(1L);
+        baseInfoDetailDTO.setParentId(10L);
+
+        InputDTO<BaseInfoDetailDTO> inputDTO = new InputDTO<>();
+        inputDTO.setActionType(EN_ACTION_TYPE.SAVE);
+        inputDTO.setUserId(17L);
+        inputDTO.setData(baseInfoDetailDTO);
+
+        when(baseInfoRepository.findById(any())).thenReturn(Optional.of(baseInfo));
+        when(baseInfoDetailRepository.findById(any())).thenReturn(Optional.empty());
+
+        assertThrows(BusinessException.class, () -> service.save(inputDTO));
+        verify(baseInfoDetailRepository, never()).save(any());
+    }
+
+    @Test
+    void testFindAllByBaseInfoId() {
+        List<BaseInfoDetail> baseInfoDetaiList = Arrays.asList(BaseInfoDetail.builder().id(1000L).title("samand").code("IK-SMD").build(),
+                BaseInfoDetail.builder().id(1000L).title("dena").code("IK-DNA").build());
+        when(baseInfoDetailRepository.findBaseInfoDetailByBaseInfo_IdOrderByCreateDateDesc(any())).thenReturn(baseInfoDetaiList);
+        List<BaseInfoDetailDTO> result = service.findAllByBaseInfoId(1L);
+        assertEquals(result.size(), baseInfoDetaiList.size());
+        assertEquals(result.get(0).getId(), baseInfoDetaiList.get(0).getId());
+        verify(baseInfoDetailRepository).findBaseInfoDetailByBaseInfo_IdOrderByCreateDateDesc(1L);
+
+    }
+
+    @Test
+    void testFindALlChild(){
+        BaseInfoDetail baseInfoDetail = BaseInfoDetail.builder().id(1000L).title("samand").code("IK-SMD").build();
+
+        when(baseInfoDetailRepository.findById(anyLong())).thenReturn(Optional.of(baseInfoDetail));
+
+        List<BaseInfoDetailDTO> allChild = service.findAllChild(1L);
+        assertNull(allChild);
+        verify(baseInfoDetailRepository).findById(1L);
     }
 
 }
