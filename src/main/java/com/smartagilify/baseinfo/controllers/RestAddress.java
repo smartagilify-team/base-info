afde6bfd446a1677b83d84c8adb9606044bc9137
amirfathi37 <fathiamir37@gmail.com>
2023-05-13 12:36:26 +0430

    chore(api-addresses): add versioning

--- src/main/java/com/smartagilify/baseinfo/controllers/RestAddress.java
+++ src/main/java/com/smartagilify/baseinfo/controllers/RestAddress.java
@@ -1,8 +1,10 @@
 package com.smartagilify.baseinfo.controllers;
 
 public class RestAddress {
-    public static final String BASE_INFO = "/api/base-info";
-    public static final String BASE_INFO_DETAIL = "/api/base-info-detail";
+
+    public static final String VERION = "/v1";
+    public static final String BASE_INFO = "/api" + VERION + "/base-info";
+    public static final String BASE_INFO_DETAIL = "/api" + VERION + "/base-info-detail";
     public static final String FIND_BY_CODE = "/find-by-code/{code}";
     public static final String FIND_ALL_BY_BASE_INFO_ID = "/find-all-by-base-info-id/{baseInfoId}";
     public static final String FIND_ALL_CHILD = "/find-all-child/{baseInfoEntityId}";
